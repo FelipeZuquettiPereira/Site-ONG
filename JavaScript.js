c168a7db7345c63f4931340dee80c172aca0d7d2
FelipeZuquettiPereira <felipe_z_pereira@estudante.sesisenai.org.br>
2026-04-10 12:21:04 -0300

    ainda não funciona

--- JavaScript.js
+++ JavaScript.js
@@ -2,11 +2,25 @@ document.getElementById(AdocaoPinguins).addEventListener("submit", function(e){
     e.preventDefault();
 
     let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").email;
-    let idade = document.getElementById("idade").idade;
+    let email = document.getElementById("email").value;
+    let idade = document.getElementById("idade").value;
+    let cpf = document.getElementById("cpf").value;
+    let pet = document.querySelector("pet").value;
+    let contato = document.getElementById("contato").value;
+    let cidade = document.getElementById("cidade").value;
+    let Moradia = document.querySelector("Moradia").value;
+    let Quintal = document.querySelector("Quintal").value;
+    let tempo_sozinho = document.getElementById("tempo_sozinho").value;
+    let motivoAdoção = document.getElementById("motivoAdoção").value;
+    let Termos = document.getElementById("Termos").checkef;
 
     if(nome.length < 3) return alert("Nome inválido(ter no mínimo 3 caracteres");
+    if(!email.includes("@")) return alert("Email Inválido");
+    if(idade < 18) return alert("Você não tem idade para adotar um pinguin");
+    if(cpf.length < 11) return alert("cpf inválido");
 
-    document.getElementById("resultado").innerHTML = "Cadastro realizado legal!<br>" + "Nome: " + nome;
+    document.getElementById("resultado").innerHTML = "Dados enviados com sucesso meu nobre"
 
-});123
+
+    console.log("resultado");
+});
